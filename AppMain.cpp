13b40b0411a5be0f21ef68038caf04a42069e9e2
250190 <250190@jc-21.jp>
2026-02-16 20:19:28 +0900

    ＜追加＞　オブジェクトを描画できるようになりました

--- AppMain.cpp
+++ AppMain.cpp
@@ -1,6 +1,14 @@
 #include <DxLib.h>
+#include "BoxObject.h"
+#include <vector>
+#include "ObjectManager.h"
 
 int InitApp();
+
+namespace {
+    ObjectManager objManager = ObjectManager::GetInstance();
+    BoxObject* boxObj = new BoxObject(Location2D(100.0f, 100.0f ), Vector2D(0.0f, 0.0f));
+}
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 	if (InitApp() != 0) {
@@ -10,11 +18,15 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	while (true) {
 		ClearDrawScreen();
 
+        objManager.UpdateObjects();
+        objManager.DrawObjects();
+
+        ScreenFlip();
+        WaitTimer(16);
 		if (ProcessMessage() == -1) {
 			break;
 		}
 	}
-
 	DxLib_End();
 	return 0;
 }
@@ -34,6 +46,8 @@ int InitApp() {
 		return -1;
 	}
 	SetDrawScreen(DX_SCREEN_BACK);
+
+    objManager.AddObject(boxObj);
 
 	return 0;
 }
