1d27be4de8755c1a001edb00a8705e2f42ac4e99
250190 <250190@jc-21.jp>
2026-02-16 20:50:41 +0900

    ＜追加＞ 座標クラスに関数を追加しました

--- Library/Location2D.cpp
+++ Library/Location2D.cpp
@@ -1,4 +1,5 @@
 #include "Location2D.h"
+#include <cmath>
 
 Location2D::Location2D() {
     x_ = 0.0f;
@@ -8,4 +9,24 @@ Location2D::Location2D() {
 Location2D::Location2D(float x, float y) {
     x_ = x;
     y_ = y;
+}
+
+float Location2D::Distance(const Location2D& otherLoc) {
+    return sqrt((x_ - otherLoc.x_) * (x_ - otherLoc.x_) +
+                (y_ - otherLoc.y_) * (y_ - otherLoc.y_));
+}
+
+Location2D Location2D::operator+(const Location2D& otherLoc) {
+    Location2D result = Location2D(x_ + otherLoc.x_, y_ + otherLoc.y_);
+    return result;
+}
+
+Location2D Location2D::operator-(const Location2D& otherLoc) {
+    Location2D result = Location2D(x_ - otherLoc.x_, y_ - otherLoc.y_);
+    return result;
+}
+
+Location2D Location2D::operator*(const float& scalar) {
+    Location2D result = Location2D(x_ * scalar, y_ * scalar);
+    return result;
 }
--- Library/Location2D.h
+++ Library/Location2D.h
@@ -7,5 +7,34 @@ public:
 
     Location2D();
     Location2D(float x, float y);
+
+    /// <summary>
+    /// 座標同士の距離を測る関数。
+    /// </summary>
+    /// <param name="otherLoc">測りたいLocation2Dを入れる。</param>
+    /// <returns></returns>
+    float Distance(const Location2D& otherLoc);
+
+    /// <summary>
+    /// 座標同士を加算する関数。
+    /// </summary>
+    /// <param name="otherLoc">加算したいLocation2Dを入れる。</param>
+    /// <returns></returns>
+    Location2D operator+(const Location2D& otherLoc);
+
+    /// <summary>
+    /// 座標同士を減算する関数。
+    /// </summary>
+    /// <param name="otherLoc">減算したいLocation2Dを入れる。</param>
+    /// <returns></returns>
+    Location2D operator-(const Location2D& otherLoc);
+
+    /// <summary>
+    /// 座標同士をスカラー倍する関数。
+    /// </summary>
+    /// <param name="scalar">スカラー倍したいLocation2Dを入れる。</param>
+    /// <returns></returns>
+    Location2D operator*(const float& scalar);
+ 
 };
 
