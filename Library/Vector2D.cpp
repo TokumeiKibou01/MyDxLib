9b3412c864a0a0227a6c8cef3d6e1be9d570da9c
250190 <250190@jc-21.jp>
2026-02-18 10:06:56 +0900

    ＜追加＞ ベクトルの関数を追加しました

--- Library/Vector2D.cpp
+++ Library/Vector2D.cpp
@@ -1,4 +1,5 @@
 #include "Vector2D.h"
+#include <cmath>
 
 Vector2D::Vector2D() {
     x_ = 0.0f;
@@ -8,4 +9,59 @@ Vector2D::Vector2D() {
 Vector2D::Vector2D(float x, float y) {
     x_ = x;
     y_ = y;
+}
+
+float Vector2D::Length() {
+    return sqrt(x_ * x_ + y_ + y_);
+}
+
+Vector2D Vector2D::Add(const Vector2D& other) {
+    Vector2D result;
+    result.x_ = x_ + other.x_;
+    result.y_ = y_ + other.y_;
+    return result;
+}
+
+Vector2D Vector2D::Subtract(const Vector2D& other) {
+    Vector2D result;
+    result.x_ = x_ - other.x_;
+    result.y_ = y_ - other.y_;
+    return result;
+}
+
+Vector2D Vector2D::Dot(const Vector2D& other) {
+    Vector2D result;
+    result.x_ = x_ * other.x_;
+    result.y_ = y_ * other.y_;
+    return result;
+}
+
+Vector2D Vector2D::Cross(const Vector2D& other) {
+    Vector2D result;
+    result.x_ = x_ * other.y_;
+    result.y_ = y_ * other.x_;
+    return result;
+}
+
+Vector2D Vector2D::Normalize() {
+    float len = Length();
+    return {
+        x_ / len,
+        y_ / len
+    };
+}
+
+Vector2D Vector2D::operator+(const Vector2D& other) {
+    return Add(other);
+}
+
+Vector2D Vector2D::operator-(const Vector2D& other) {
+    return Subtract(other);
+}
+
+Vector2D Vector2D::FromAngle(float angle) {
+    return {
+        cos(angle),
+        sin(angle)
+    };
 }
--- Library/Vector2D.h
+++ Library/Vector2D.h
@@ -7,5 +7,35 @@ public:
 
     Vector2D();
     Vector2D(float x, float y);
+
+    float Length();
+
+    Vector2D Add(const Vector2D& other);
+    Vector2D Subtract(const Vector2D& other);
+    Vector2D Dot(const Vector2D& other);
+    Vector2D Cross(const Vector2D& other);
+    Vector2D Normalize();
+
+    /// <summary>
+    /// 加算する関数（Addと同じ）
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    Vector2D operator+(const Vector2D& other);
+
+    /// <summary>
+    /// 減算する関数（Subtractと同じ）
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    Vector2D operator-(const Vector2D& other);
+
+    /// <summary>
+    /// 角度から単位ベクトルを作る
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    static Vector2D FromAngle(float angle);
+    
 };
 
